36a14f02744bdcc377ac296083701a00cdc4859a
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2023-12-16 17:52:20 -0800

    boomshakalaka

--- netlify/functions/youtubeDownloader.js
+++ netlify/functions/youtubeDownloader.js
@@ -1,75 +1,73 @@
-const ytdl = require('ytdl-core');
-const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
-const ffmpegPath = require('ffmpeg-static');
+// Import required modules
+import ytdl from 'ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
+import ffmpegPath from 'ffmpeg-static';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getStorage } from 'firebase-admin/storage';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { createWriteStream } from 'fs';
 
 // Set the path for the ffmpeg binary
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-async function downloadAndConvert(videoUrl, outputPath) {
-    try {
-        // Fetch video information
-        const videoInfo = await ytdl.getInfo(videoUrl);
+// Firebase Admin Initialization
+const firebaseConfig = {
+    credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+};
+initializeApp(firebaseConfig);
 
-        // Choose the highest quality audio format
-        const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+const storage = getStorage();
 
-        // Download video stream
-        const videoStream = ytdl(videoUrl, { format: videoFormat });
+// Function to download and convert video
+async function downloadAndConvert(videoUrl) {
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+    const videoStream = ytdl(videoUrl, { format: videoFormat });
+    const outputPath = join(tmpdir(), `${Date.now()}_${Math.random().toString(36).substring(2, 8)}.mp3`);
 
-        // Convert video to mp3
-        await new Promise((resolve, reject) => {
-            ffmpeg()
-                .input(videoStream)
-                .audioCodec('libmp3lame')
-                .audioBitrate(320)
-                .output(outputPath)
-                .on('end', resolve)
-                .on('error', (err) => {
-                    console.error('Error in ffmpeg:', err);
-                    reject(err);
-                })
-                .run();
-        });
-
-        return path.basename(outputPath);
-    } catch (error) {
-        console.error('Error in downloadAndConvert:', error);
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        ffmpeg()
+            .input(videoStream)
+            .audioCodec('libmp3lame')
+            .audioBitrate(320)
+            .output(outputPath)
+            .on('end', () => resolve(outputPath))
+            .on('error', reject)
+            .run();
+    });
 }
 
-exports.handler = async function (event, context) {
+// Function to upload file to Firebase Storage
+async function uploadToFirebaseStorage(filePath, fileName) {
+    const bucket = storage.bucket();
+    await bucket.upload(filePath, {
+        destination: fileName,
+    });
+    const [url] = await bucket.file(fileName).getSignedUrl({
+        action: 'read',
+        expires: '03-09-2491', // Set an appropriate expiry date
+    });
+    return url;
+}
+
+// Netlify function handler
+export async function handler(event) {
+    if (!event.body) {
+        console.error('Request body is empty.');
+        return { statusCode: 400, body: JSON.stringify({ error: 'Bad Request' }) };
+    }
+
     try {
-        // Check if event.body is not empty
-        if (!event.body) {
-            console.error('Request body is empty.');
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: 'Bad Request' }),
-            };
-        }
-
         const { videoUrl } = JSON.parse(event.body);
+        const outputPath = await downloadAndConvert(videoUrl);
+        const fileName = outputPath.split('/').pop();
+        const downloadUrl = await uploadToFirebaseStorage(outputPath, fileName);
 
-        // Generate a unique file name
-        const fileName = `${Date.now()}_${Math.random().toString(36).substring(2, 8)}.mp3`;
-
-        // Set the output path directly to the public/downloads folder
-        const outputPath = path.resolve('public/downloads', fileName);
-
-        // Perform the download and conversion
-        await downloadAndConvert(videoUrl, outputPath);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ downloadLink: `/downloads/${fileName}` }),
-        };
+        return { statusCode: 200, body: JSON.stringify({ downloadLink: downloadUrl }) };
     } catch (error) {
         console.error('Error in download function:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' }),
-        };
+        return { statusCode: 500, body: JSON.stringify({ error: 'Internal Server Error' }) };
     }
 };
